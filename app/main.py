3bab720c41842b5ec3ab629e96f92733fd78c79a
Your Name <you@example.com>
2023-02-27 11:39:10 +0200

    add index.html

--- app/main.py
+++ app/main.py
@@ -1,16 +1,24 @@
 from typing import Union
 
 from fastapi import FastAPI
-from app.endpoints import hello_world
+
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return hello_world()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# from app.endpoints import hello_world
+
+# app = FastAPI()
+
+
+# @app.get("/")
+# def read_root():
+#     return hello_world()
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
