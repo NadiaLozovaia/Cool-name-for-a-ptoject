555485734522a3c6b9d178620701243c39b3dad0
Your Name <you@example.com>
2023-02-28 12:21:09 +0200

    html/js experiments

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,2 +1,7 @@
+import json
+
 def hello_world():
     return {"Hello": "World"}
+
+def get_users():
+    return json.loads(open("users.json").read())
--- app/main.py
+++ app/main.py
@@ -1,24 +1,30 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from fastapi.staticfiles import StaticFiles
 
+from fastapi.responses import HTMLResponse, FileResponse
+
+from fastapi.templating import Jinja2Templates
+
+from app.endpoints import get_users
+
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="static/html", html=True), name="static")
+# app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
+templates = Jinja2Templates(directory="static/templates")
 
-# from app.endpoints import hello_world
+@app.get("/first", response_class=HTMLResponse)
+async def first(request: Request):
+    users = get_users()
+    return templates.TemplateResponse("item.html", {"request": request, "users": users})
 
-# app = FastAPI()
+@app.get("/users")
+def read_users():
+    return get_users()
 
-
-# @app.get("/")
-# def read_root():
-#     return hello_world()
-
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
+@app.get("/second")
+async def main():
+    return FileResponse("static/html/index.html")
