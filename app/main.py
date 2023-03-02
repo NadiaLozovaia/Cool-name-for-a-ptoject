71e1ddaecc070797c14dbf137ae29f1be8f96bba
Your Name <you@example.com>
2023-03-02 12:08:45 +0200

    add db init script

--- app/main.py
+++ app/main.py
@@ -4,21 +4,9 @@ from fastapi import FastAPI
 
 from fastapi.staticfiles import StaticFiles
 
+
 app = FastAPI()
 
 app.mount("/", StaticFiles(directory="static/html", html=True), name="static")
 
 
-# from app.endpoints import hello_world
-
-# app = FastAPI()
-
-
-# @app.get("/")
-# def read_root():
-#     return hello_world()
-
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
